32677b58ea49df14ce5aba0a5403385221c8ba22
Pavel Ilin <pavel_ilin@yahoo.com>
2019-09-02 19:59:54 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -14,13 +14,19 @@ end
 cart_hash
 end
 
+
+
 def apply_coupons(cart, coupons)
   # code here
 end
 
+
+
 def apply_clearance(cart)
   # code here
 end
+
+
 
 def checkout(cart, coupons)
   # code here
