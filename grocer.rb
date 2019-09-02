7093fb26105f72450916caefad037e3e5322ca6d
Pavel Ilin <pavel_ilin@yahoo.com>
2019-09-02 19:52:26 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,12 +1,17 @@
 def consolidate_cart(cart)
   cart_hash = {}
-  cart.each do |items_array|
-    items_array.each do |item, attribute_hash|
-      cart_hash[item] = attribute_hash
-      cart_hash[item][:count] ? cart_hash[item][:count] += 1 : cart_hash[item][:count] = 1
+  cart.each do |x| 
+    if cart_hash[x.keys[0]]
+      cart_hash[x.keys[0]][:count]+=1 
+    else
+      cart_hash[x.keys[0]] = {
+        :price => x.values[0][:price],
+        :clearance => x.values[0][:clearance],
+        :count => 1
+      }
     end
-  end
-  return cart_hash
+end
+cart_hash
 end
 
 def apply_coupons(cart, coupons)
